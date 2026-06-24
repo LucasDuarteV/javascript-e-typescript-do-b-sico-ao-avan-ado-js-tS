3a525b4084070e44fbca0d7fa3087a8bf62d9a57
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-24 16:42:17 -0300

    Exercícios - Const e Let

---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Luiz Ótavio'
+const sobrenome = 'Miranda'
+const idade = 30
+const peso = 84;
+const altura = 1.80
+let imc = peso / (altura * altura)
+let anoDeNascimento = 2010 - 30
+
+console.log(`${nome} ${sobrenome} tem ${idade}, pesa ${peso} kg tem ${altura} e seu IMC é de ${imc}`)
+console.log(`${nome} ${sobrenome} nasceu em ${anoDeNascimento}`)
