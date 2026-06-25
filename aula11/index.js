96ab3b29d465872c59fb9759f753a0598dfdcc36
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-25 11:12:25 -0300

    Alert, Confirm e Prompt (Navegador)

---
+++ aula11/index.js
@@ -0,0 +1,8 @@
+let n1 = prompt('Digite um numero: ')
+let n2 = prompt('Digite o segundo numero: ')
+n1 = parseInt(n1)
+n2 = parseInt(n2)
+
+let soma = n1 + n2
+
+alert(`O resulda da soma é: ${soma}`)
