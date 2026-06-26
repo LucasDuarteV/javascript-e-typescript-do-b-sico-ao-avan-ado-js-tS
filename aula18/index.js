785f4876890244574548f0a27feeb8fd5a9bcba9
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-26 10:48:29 -0300

    Objetos (Básico)

---
+++ aula18/index.js
@@ -0,0 +1,23 @@
+function criarPessoa(nome , sobrenome, idade){
+return{
+    Nome: nome,
+    Sobrenome: sobrenome,
+    Idade: idade
+}
+}
+
+const pessoaUm = criarPessoa('Lucas' , 'Duarte' , 22)
+console.log(pessoaUm.Nome)
+
+const pessoa1 = {
+    nome: 'Luiz',
+    sobrenome: 'Miranda',
+    idade: 25,
+
+    fala(){
+        console.log(`A minha idade é ${this.idade}`)
+    },
+    IncrementaIdade(){
+        this.idade++; 
+    }
+}
