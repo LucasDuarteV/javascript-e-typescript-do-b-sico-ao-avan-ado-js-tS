456b8d6e77d5e0cd6bc8349187d209feb5fcce14
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-30 13:54:13 -0300

    Atribuição via desestruturação (Objetos)

---
+++ aula33/index.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Duarte',
+    idade: 22,
+    endereco: {
+        rua: 'Santiago Bernabeu',
+        numero: 12
+    }
+}
+
+const {nome} = pessoa
+console.log(nome)
