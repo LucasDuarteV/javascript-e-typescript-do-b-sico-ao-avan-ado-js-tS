2e8a6d3c07e39925a804b46fab387dc5e2b37de5
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-02 15:46:43 -0300

    Funções construtoras (constructor functions)

---
+++ aula58/index.js
@@ -0,0 +1,20 @@
+function Pessoa(nome , sobrenome){
+
+    const id = 123456
+
+    const metodoInterno = function(){
+        
+    }
+
+    this.nome = nome
+    this.sobrenome = sobrenome
+
+    this.metodo = function(){
+        console.log(this.nome + ': sou um método')
+    }
+
+}
+
+const p1 = new Pessoa('Lucas' , 'Duarte')
+console.log(p1)
+console.log(p1.metodo())
