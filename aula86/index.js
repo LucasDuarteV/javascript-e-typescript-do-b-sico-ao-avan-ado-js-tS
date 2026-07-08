d82519721e313468c5ff963539fced65117ef4b6
LucasDuarteV <lucasdv2733@gmail.com>
2026-07-08 12:03:49 -0300

    Promises

---
+++ aula86/index.js
@@ -0,0 +1,33 @@
+function rand(min, max){
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log(msg)
+            resolve(msg)
+        }, tempo)
+    })
+}
+
+esperaAi('Conexão com o BD', rand(1, 3))
+    .then(resposta =>{
+        console.log(resposta)
+        return esperaAi('Buscando dados da BASE', rand(1, 3))
+    })
+    .then(resposta =>{
+        console.log(resposta)
+        return esperaAi('Tratando os dados da BASE', rand(1, 3))
+    })
+    .then(resposta =>{
+        console.log(resposta)
+    })
+    .then(()=>{
+        console.log('Exibe dados na tela')
+    })
+    .catch(erro =>{
+        console.log('Erro:', erro)
+    })
